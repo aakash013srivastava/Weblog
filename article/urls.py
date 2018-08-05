e3d2aee64f6ac6bff215619ded337701977c93ef
Aakash Srivastava <aakashsrivastava013@gmail.com>
2018-08-05 19:23:29 +0530

    Logout facilitated and changes made to navigation bar.

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
+
 
 app_name = 'article'
 
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^create/$',views.create,name='create'),
     url(r'^signup/$',views.signup,name='signup'),
     url(r'^login/$',views.user_login,name ='user_login'),
+    url(r'^logout/$',views.user_logout,name ='user_logout'),
 ]
--- article/views.py
+++ article/views.py
@@ -2,7 +2,7 @@ from .models import Article,Comment
 from django.shortcuts import render,get_object_or_404,redirect
 from .forms import ArticleForm,CommentForm,LoginForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 
 
 
@@ -11,27 +11,34 @@ def index(request):
     return render(request,'article/index.html',{'articles':articles})
 
 def detail(request,article_id):
-    article = get_object_or_404(Article,pk=article_id)
-    comment_form = CommentForm()
-
-    if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment_text = comment_form.cleaned_data['comment_text']
-            comment = Comment()
-            comment.comment_text = comment_text
-            comment.comment_author = request.user.username
-            comment.article = Article.objects.get(pk=article_id)
-            comment.save()
-    else:
+    if request.user.username:
+        article = get_object_or_404(Article,pk=article_id)
         comment_form = CommentForm()
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment_text = comment_form.cleaned_data['comment_text']
+                comment = Comment()
+                comment.comment_text = comment_text
+                comment.comment_author = request.user.username
+                comment.article = Article.objects.get(pk=article_id)
+                comment.save()
+        else:
+            comment_form = CommentForm()
 
-    return render(request,'article/detail.html',{'article':article,'comment_form':comment_form})
+        return render(request,'article/detail.html',{'article':article,'comment_form':comment_form})
+
+    else:
+        form = LoginForm()
+        return render(request,'article/login.html',{'form':form})
 
 def new(request):
-    form = ArticleForm()
-    return render(request,'article/new.html',{'form':form})
-
+    if request.user.username:
+        form = ArticleForm()
+        return render(request,'article/new.html',{'form':form})
+    else:
+        form = LoginForm()
+        return render(request,'article/login.html',{'form':form})
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
@@ -63,16 +70,25 @@ def signup(request):
     return render(request,'article/signup.html',{'form':form})
 
 def user_login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            raw_password = form.cleaned_data['password']
-            user = authenticate(username=username,password=raw_password)
-            login(request,user)
-            article = Article.objects.all()
-            return render(request,'article/index.html',{'articles':article})
-    else:
-        form = LoginForm()
+    if not request.user.username:
+        if request.method == "POST":
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                raw_password = form.cleaned_data['password']
+                user = authenticate(username=username,password=raw_password)
+                login(request,user)
+                article = Article.objects.all()
+                return render(request,'article/index.html',{'articles':article})
+        else:
+            form = LoginForm()
 
-    return render(request,'article/login.html',{'form':form})
+        return render(request,'article/login.html',{'form':form})
+    else:
+        articles = Article.objects.all()
+        return render(request,'article/index.html',{'articles':articles})
+
+def user_logout(request):
+    logout(request)
+    article = Article.objects.all()
+    return render(request,'article/index.html',{'articles':article})
